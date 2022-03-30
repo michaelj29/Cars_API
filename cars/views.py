4ee39a8f2ce7d5f556faff3bdb8bfa9814678080
Michael J <johnson_michael1329@yahoo.com>
2022-03-30 16:42:57 -0500

    registered car model with admin site + complete POST functionality for car in view.py + updated Framework decimal settings.

--- cars/views.py
+++ cars/views.py
@@ -2,11 +2,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import CarSerializer
 from .models import Car
+from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
-  cars = Car.objects.all()
-
-  serializer = CarSerializer(cars, many=True)
-
-  return Response(serializer.data)
+  if request.method == 'GET':
+   cars = Car.objects.all()
+   serializer = CarSerializer(cars, many=True)
+   return Response(serializer.data)
+  elif request.method == 'POST':
+    serializer = CarSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
--- cars_project/settings.py
+++ cars_project/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'cars',
 ]
 
+REST_FRAMEWORK = {
+  'COERCE_DECIMAL_TO_STRING': False
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
