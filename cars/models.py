813aca1166d0b02529a80c00c78987cb92fe86f8
Michael J <johnson_michael1329@yahoo.com>
2022-03-30 16:12:57 -0500

    debug and fix and error where the database did not exist when called.

--- cars/models.py
+++ cars/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Cars(models.Model):
+class Car(models.Model):
     make = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     year = models.IntegerField()
